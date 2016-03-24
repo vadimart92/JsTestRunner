d37be21c90228e37041d8eac54f1bd45225a71e1
Vadym <vadimart92@gmail.com>
2016-03-24 20:48:30 +0200

    build update

--- Buid/RunServer.bat
+++ Buid/RunServer.bat
@@ -1,2 +1,2 @@
 cd bin
-JsTestRunner.Server.exe -p=53853 -url="http://localhost:2376/WebApp780/0/TestTools/Tests.aspx?useCoreBundle=true&harness.enableCodeCoverage=true&harness.contentManagerClass=Terrasoft.Tests.ContentManager&harness.sandbox=false"
+JsTestRunner.Server.exe -p=53853 -url="http://localhost:2376/WebApp780/0/TestTools/Tests.aspx?useCoreBundle=true&harnessMode=coverage"
