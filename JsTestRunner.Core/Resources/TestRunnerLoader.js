4b0e28ec92730aa4c07b593e3b7ef0c851301cb1
Vadym <vadimart92@gmail.com>
2016-04-01 23:34:47 +0300

    loader

--- JsTestRunner.Core/Resources/TestRunnerLoader.js
+++ JsTestRunner.Core/Resources/TestRunnerLoader.js
@@ -2,3 +2,32 @@
 
 var baseUri = "http://localhost:53853/";
 
+loadfiles([
+	"Resources/jquery.signalR-2.2.0.js",
+	"signalr/hubs",
+	"Resources/JsTestRunner.js",
+	"Resources/Bootstrap.js"
+]);
+
+function loadfiles(filenames) {
+	var start;
+	var queue = start = jQuery.Deferred();
+	for (var index in filenames) {
+		if (filenames.hasOwnProperty(index)) {
+			queue = addToQueue(queue, filenames[index]);;
+		}
+	}
+	start.resolve();
+}
+
+function addToQueue(queue, url) {
+	var next = jQuery.Deferred();
+	queue.then(function () {
+		var src = baseUri + url;
+		jQuery.getScript(src, function () {
+			next.resolve();
+		});
+	});
+	return next;
+}
+
